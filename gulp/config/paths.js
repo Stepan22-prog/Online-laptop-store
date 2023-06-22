e2d3aea3514e5219935574c373e2c4b2e15a4737
Stepan <stepanigcom@gmail.com>
2023-06-22 18:49:33 +0300

    The project was init

---
+++ gulp/config/paths.js
@@ -0,0 +1,39 @@
+import path from 'path';
+
+const projectDirName = path.basename(path.resolve());
+const buildFolder = `./dist`;
+const srcFolder = `./src`;
+
+const filePaths = {
+  build: {
+    js: `${buildFolder}/js/`,
+    css: `${buildFolder}/css/`,
+    html: `${buildFolder}/`,
+    img: `${buildFolder}/img/`,
+    fonts: `${buildFolder}/fonts/`,
+    static: `${buildFolder}/static/`,
+  },
+  src: {
+    js: `${srcFolder}/js/app.js`,
+    img: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+    svg: `${srcFolder}/img/**/*.svg`,
+    scss: `${srcFolder}/scss/style.scss`,
+    html: `${srcFolder}/*.html`,
+    static: `${srcFolder}/static/**/*.*`,
+    svgIcons: `${srcFolder}/icons/*.svg`,
+  },
+  watch: {
+    js: `${srcFolder}/js/**/*.js`,
+    scss: `${srcFolder}/scss/**/*.scss`,
+    html: `${srcFolder}/**/*.html`,
+    img: `${srcFolder}/**/*.{jpg,jpeg,png,svg,gif,webp,ico}`,
+    static: `${srcFolder}/static/**/*.*`,
+  },
+  clean: buildFolder,
+  buildFolder: buildFolder,
+  srcFolder: srcFolder,
+  projectDirName,
+  ftp: ``, // Путь к нужной папке на удаленном сервере. Gulp добавит имя папки проекта автоматически
+};
+
+export { filePaths };
