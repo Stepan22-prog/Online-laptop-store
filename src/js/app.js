e30de98f011d3d71e255034bd6b841212286fc18
Stepan <stepanigcom@gmail.com>
2023-06-30 11:49:02 +0300

    Filter block and selctor for sort for mobile version is ready. NoUiSlider and wNumb lib were added.

--- src/js/app.js
+++ src/js/app.js
@@ -138,3 +138,39 @@ btn.addEventListener('click', () => {
     behavior: "smooth"
   });
 })
+import './modules/select.js';
+
+import noUiSlider from 'nouislider';
+import './libs/wNumb.min.js'
+const html5Slider = document.getElementById('range');
+const minPrice = document.getElementById('minPrice');
+const maxPrice = document.getElementById('maxPrice');
+
+noUiSlider.create(html5Slider, {
+  start: [110, 900],
+  connect: true,
+  range: {
+    'min': 100,
+    'max': 1000
+  },
+  format: wNumb({
+    decimals: 0,
+  })
+});
+
+html5Slider.noUiSlider.on('update', function (values, handle) {
+
+  let value = values[handle];
+
+  if (handle) {
+    maxPrice.value = value;
+  } else {
+    minPrice.value = value
+  }
+});
+minPrice.addEventListener('change', function () {
+  html5Slider.noUiSlider.set([this.value, null]);
+});
+maxPrice.addEventListener('change', function () {
+  html5Slider.noUiSlider.set([null, this.value]);
+});
