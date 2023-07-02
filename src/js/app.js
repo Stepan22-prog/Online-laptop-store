637da8fc266a736a2d5ee262fe24c5e2c5f7a1ae
Stepan <stepanigcom@gmail.com>
2023-07-02 13:48:53 +0300

    The mobile version of gallery and product-text are ready

--- src/js/app.js
+++ src/js/app.js
@@ -8,11 +8,11 @@
  */
 // import MousePRLX from './libs/parallaxMouse'
 // import AOS from 'aos'
-import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
+import Swiper, { Autoplay, Navigation, Pagination, Thumbs } from 'swiper';
 
 import BaseHelpers from './helpers/BaseHelpers.js';
 //import BurgerMenu from './modules/BurgerMenu';
-import Tabs from './modules/Tabs';
+//import Tabs from './modules/Tabs';
 import Accordion from './modules/Accordion.js';
 
 BaseHelpers.checkWebpSupport();
@@ -49,7 +49,7 @@ BaseHelpers.headerFixed();
  * */
 // new MousePRLX();
 
-new Tabs('tabs-example', {});
+//new Tabs('tabs-example', {});
 
 new Accordion('.accordion', {
   shouldOpenAll: false, // true
@@ -79,48 +79,81 @@ window.onload = function () {
 // angelwatt.com/coding/masked_input.php
 (function (a) { a.MaskedInput = function (f) { if (!f || !f.elm || !f.format) { return null } if (!(this instanceof a.MaskedInput)) { return new a.MaskedInput(f) } var o = this, d = f.elm, s = f.format, i = f.allowed || "0123456789", h = f.allowedfx || function () { return true }, p = f.separator || "/:-", n = f.typeon || "_YMDhms", c = f.onbadkey || function () { }, q = f.onfilled || function () { }, w = f.badkeywait || 0, A = f.hasOwnProperty("preserve") ? !!f.preserve : true, l = true, y = false, t = s, j = (function () { if (window.addEventListener) { return function (E, C, D, B) { E.addEventListener(C, D, (B === undefined) ? false : B) } } if (window.attachEvent) { return function (D, B, C) { D.attachEvent("on" + B, C) } } return function (D, B, C) { D["on" + B] = C } }()), u = function () { for (var B = d.value.length - 1; B >= 0; B--) { for (var D = 0, C = n.length; D < C; D++) { if (d.value[B] === n[D]) { return false } } } return true }, x = function (C) { try { C.focus(); if (C.selectionStart >= 0) { return C.selectionStart } if (document.selection) { var B = document.selection.createRange(); return -B.moveStart("character", -C.value.length) } return -1 } catch (D) { return -1 } }, b = function (C, E) { try { if (C.selectionStart) { C.focus(); C.setSelectionRange(E, E) } else { if (C.createTextRange) { var B = C.createTextRange(); B.move("character", E); B.select() } } } catch (D) { return false } return true }, m = function (D) { D = D || window.event; var C = "", E = D.which, B = D.type; if (E === undefined || E === null) { E = D.keyCode } if (E === undefined || E === null) { return "" } switch (E) { case 8: C = "bksp"; break; case 46: C = (B === "keydown") ? "del" : "."; break; case 16: C = "shift"; break; case 0: case 9: case 13: C = "etc"; break; case 37: case 38: case 39: case 40: C = (!D.shiftKey && (D.charCode !== 39 && D.charCode !== undefined)) ? "etc" : String.fromCharCode(E); break; default: C = String.fromCharCode(E); break }return C }, v = function (B, C) { if (B.preventDefault) { B.preventDefault() } B.returnValue = C || false }, k = function (B) { var D = x(d), F = d.value, E = "", C = true; switch (C) { case (i.indexOf(B) !== -1): D = D + 1; if (D > s.length) { return false } while (p.indexOf(F.charAt(D - 1)) !== -1 && D <= s.length) { D = D + 1 } if (!h(B, D)) { c(B); return false } E = F.substr(0, D - 1) + B + F.substr(D); if (i.indexOf(F.charAt(D)) === -1 && n.indexOf(F.charAt(D)) === -1) { D = D + 1 } break; case (B === "bksp"): D = D - 1; if (D < 0) { return false } while (i.indexOf(F.charAt(D)) === -1 && n.indexOf(F.charAt(D)) === -1 && D > 1) { D = D - 1 } E = F.substr(0, D) + s.substr(D, 1) + F.substr(D + 1); break; case (B === "del"): if (D >= F.length) { return false } while (p.indexOf(F.charAt(D)) !== -1 && F.charAt(D) !== "") { D = D + 1 } E = F.substr(0, D) + s.substr(D, 1) + F.substr(D + 1); D = D + 1; break; case (B === "etc"): return true; default: return false }d.value = ""; d.value = E; b(d, D); return false }, g = function (B) { if (i.indexOf(B) === -1 && B !== "bksp" && B !== "del" && B !== "etc") { var C = x(d); y = true; c(B); setTimeout(function () { y = false; b(d, C) }, w); return false } return true }, z = function (C) { if (!l) { return true } C = C || event; if (y) { v(C); return false } var B = m(C); if ((C.metaKey || C.ctrlKey) && (B === "X" || B === "V")) { v(C); return false } if (C.metaKey || C.ctrlKey) { return true } if (d.value === "") { d.value = s; b(d, 0) } if (B === "bksp" || B === "del") { k(B); v(C); return false } return true }, e = function (C) { if (!l) { return true } C = C || event; if (y) { v(C); return false } var B = m(C); if (B === "etc" || C.metaKey || C.ctrlKey || C.altKey) { return true } if (B !== "bksp" && B !== "del" && B !== "shift") { if (!g(B)) { v(C); return false } if (k(B)) { if (u()) { q() } v(C, true); return true } if (u()) { q() } v(C); return false } return false }, r = function () { if (!d.tagName || (d.tagName.toUpperCase() !== "INPUT" && d.tagName.toUpperCase() !== "TEXTAREA")) { return null } if (!A || d.value === "") { d.value = s } j(d, "keydown", function (B) { z(B) }); j(d, "keypress", function (B) { e(B) }); j(d, "focus", function () { t = d.value }); j(d, "blur", function () { if (d.value !== t && d.onchange) { d.onchange() } }); return o }; o.resetField = function () { d.value = s }; o.setAllowed = function (B) { i = B; o.resetField() }; o.setFormat = function (B) { s = B; o.resetField() }; o.setSeparator = function (B) { p = B; o.resetField() }; o.setTypeon = function (B) { n = B; o.resetField() }; o.setEnabled = function (B) { l = B }; return r() } }(window));
 
-const swiper = new Swiper('.swiper', {
-  // configure Swiper to use modules
-  modules: [Navigation, Pagination, Autoplay],
-  slidesPerView: 1,
-  slidesPerGroup: 1,
-  spaceBetween: 10,
-  speed: 800,
-  grabCursor: true,
-  breakpoints: {
-    // when window width is >= 500px
-    500: {
-      slidesPerGroup: 2,
-      slidesPerView: 2
+//sliders
+if (document.querySelector('.new__slider')) {
+  const swiper = new Swiper('.new__slider', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination, Autoplay],
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 10,
+    speed: 800,
+    grabCursor: true,
+    breakpoints: {
+      // when window width is >= 500px
+      500: {
+        slidesPerGroup: 2,
+        slidesPerView: 2
+      },
+      // when window width is >= 640px
+      768: {
+        slidesPerGroup: 3,
+        slidesPerView: 3
+      },
+      1024: {
+
+      },
+      1200: {
+        slidesPerGroup: 4,
+        slidesPerView: 4,
+        spaceBetween: 15
+      },
     },
-    // when window width is >= 640px
-    768: {
-      slidesPerGroup: 3,
-      slidesPerView: 3
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
     },
-    1024: {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 3000,
+    },
+  });
+}
+if (document.querySelector('.gallery__slider')) {
+  const swiperThumb = new Swiper('.thumbs__slider', {
+    // configure Swiper to use modules
+    modules: [Navigation],
+    slidesPerView: 3,
+    slidesPerGroup: 1,
+    spaceBetween: 10,
+    speed: 800,
+    breakpoints: {
 
     },
-    1200: {
-      slidesPerGroup: 4,
-      slidesPerView: 4,
-      spaceBetween: 15
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  autoplay: {
-    delay: 3000,
-  },
-});
+  });
+  const swiper = new Swiper('.gallery__slider', {
+    // configure Swiper to use modules
+    modules: [Thumbs],
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 10,
+    speed: 800,
+    breakpoints: {
 
+    },
+    thumbs: {
+      swiper: swiperThumb,
+    },
+  });
+}
 //scroll
 const btn = document.querySelector('.to-top');
 document.addEventListener("scroll", () => {
@@ -138,39 +171,48 @@ btn.addEventListener('click', () => {
     behavior: "smooth"
   });
 })
+
+//select
 import './modules/select.js';
 
+//range
 import noUiSlider from 'nouislider';
 import './libs/wNumb.min.js'
-const html5Slider = document.getElementById('range');
-const minPrice = document.getElementById('minPrice');
-const maxPrice = document.getElementById('maxPrice');
+if (document.getElementById('range')) {
 
-noUiSlider.create(html5Slider, {
-  start: [110, 900],
-  connect: true,
-  range: {
-    'min': 100,
-    'max': 1000
-  },
-  format: wNumb({
-    decimals: 0,
-  })
-});
+  const html5Slider = document.getElementById('range');
+  const minPrice = document.getElementById('minPrice');
+  const maxPrice = document.getElementById('maxPrice');
 
-html5Slider.noUiSlider.on('update', function (values, handle) {
+  noUiSlider.create(html5Slider, {
+    start: [110, 900],
+    connect: true,
+    range: {
+      'min': 100,
+      'max': 1000
+    },
+    format: wNumb({
+      decimals: 0,
+    })
+  });
 
-  let value = values[handle];
+  html5Slider.noUiSlider.on('update', function (values, handle) {
 
-  if (handle) {
-    maxPrice.value = value;
-  } else {
-    minPrice.value = value
-  }
-});
-minPrice.addEventListener('change', function () {
-  html5Slider.noUiSlider.set([this.value, null]);
-});
-maxPrice.addEventListener('change', function () {
-  html5Slider.noUiSlider.set([null, this.value]);
-});
+    let value = values[handle];
+
+    if (handle) {
+      maxPrice.value = value;
+    } else {
+      minPrice.value = value
+    }
+  });
+  minPrice.addEventListener('change', function () {
+    html5Slider.noUiSlider.set([this.value, null]);
+  });
+  maxPrice.addEventListener('change', function () {
+    html5Slider.noUiSlider.set([null, this.value]);
+  });
+}
+
+//tabs
+modules.tabs();
